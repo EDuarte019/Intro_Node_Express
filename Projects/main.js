85a3b3dffc09b5699dd3dc10b2c3e6cf43cf49ca
Evelyn <eduarte019@west-mec.org>
2019-10-22 17:14:29 -0700

    Final commit
    API working and 5 pages, using glitch.

--- Projects/main.js
+++ Projects/main.js
@@ -1,116 +1,111 @@
-
-/** BASIC EXPRESS SETUP
-* server.js
-* 1. Require Express
-* 2. Create a variable to use express
-* 3. Allow the use of static assets
-*    - examples: css, .js (client-side)
-* 4. Require the request library 
-*/
-const express = require("express");
+const express = require('express');
 const app = express();
-app.use(express.static("public"));
-var request = require("request");
-const fetch = require('node-fetch');
-
+const path = require("path")
+const fetch = require("node-fetch")
 
 fetch("https://swapi.co/api/people")
-    .then(function(response) {
-        if (response.status !== 200) {
-            console.log("Error" + response.status);
-            return;
-        }   
-        response.json()
-        .then(function(data) {
-            console.log(data);
-        });
+  .then(res => res.json())
+  .then(json => {
+    name = json.results[0].name;
+     height = json.results[0].height;
+    mass = json.results[0].mass;
+    gender = json.results[0].gender;
+
+    name1 = json.results[1].name;
+    height1 = json.results[1].height;
+   mass1 = json.results[1].mass;
+   gender1 = json.results[1].gender;
+
+   name2 = json.results[2].name;
+   height2 = json.results[2].height;
+  mass2 = json.results[2].mass;
+  gender2 = json.results[2].gender;
+
+  name3 = json.results[3].name;
+  height3 = json.results[3].height;
+ mass3 = json.results[3].mass;
+ gender3 = json.results[3].gender;
+
+ name4 = json.results[4].name;
+ height4 = json.results[4].height;
+mass4 = json.results[4].mass;
+gender4 = json.results[4].gender;
+  });
+
+  app.use(express.static(__dirname + "/public"))
+  app.set('views',path.join(__dirname,"views"))
+  app.set("view engine", "hbs")
+
+  app.get('/', (req, res) => {
+    res.render('index', {
+      "username" : name,
+      "height" : height,
+      "mass" : mass,
+      "gender" : gender,
     })
-.catch(function(err) {console.log("Fetch error" +  err);})
-app.get('/',(req, res)=> res.send(
-  app.listen(port, () => console.log('example app listening on port ${port}!'))
-))
+  })
 
-// var fetch = require("node-fetch");
-// app.get("/", (req, res) => {
-//   var people = fetch('https://swapi.co/api/people')
-//   .then(res=>res.json())
-//   .then(
-//     (json)=>{
-//       console.log(json)
-//       res.render('people')
-//     }
-//   )
-//   response.render('index', {
-//     people: "Hello world"
-//   });
-// });
+  app.get('/luke', (req, res) => {
+    res.render('luke', {
+      "username1" : name1,
+      "height1" : height1,
+      "mass1" : mass1,
+      "gender1" : gender1,
+    })
+  })
+  
+  app.get('/people', (req, res) => {
+    res.render('people', {
+      "username2" : name2,
+      "height2" : height2,
+      "mass2" : mass2,
+      "gender2" : gender2,
+    })
+  })
 
+  app.get('/about', (req, res) => {
+    res.render('about', {
+      "username3" : name3,
+      "height3" : height3,
+      "mass3" : mass3,
+      "gender3" : gender3,
+    })
+  })
 
-/**
-* http://expressjs.com/en/starter/basic-routing.html
-* @example
-  app.get("/", function(request, response) {
-    response.sendFile(__dirname + "/views/index.html");
-  });
-*/
+  app.get('/person', (req, res) => {
+    res.render('person', {
+      "username4" : name4,
+      "height4" : height4,
+      "mass4" : mass4,
+      "gender4" : gender4,
+    })
+  })
+  // app.get('/people', async (req, res) => {
+  //   const fetchPeople = await fetch("https://swapi.co/api/people");
+  //   const peopleData = await fetchPeople.json();
+    
+  //   const [luke,C3PO,R2D2,DarthVader,Leia] = peopleData.results;
 
+  //   console.log(luke);
 
+  //   res.render('index', {
+  //     people : peopleData.results,
+  //   })
+  
+  // })
 
-/** TEMPLATE ENGINE
-* Express Handlebars is setup here
-* 1. Require [jsesc](https://github.com/mathiasbynens/jsesc)
-     - This is used in the [helper](https://handlebarsjs.com/block_helpers.html)
-     - [liberary of helper function](https://github.com/helpers/handlebars-helpers)
-* 2. Define the template engine (hbs aka handlebars)
-     - The default layout is the main.hbs file
-  3. Set the "view" / "template" engine
-    - [app.engine](https://expressjs.com/en/api.html#app.engine)
-    - [app.set](https://expressjs.com/en/api.html#app.set)
-*/
-const hbs = require('express-handlebars')({
-  defaultLayout: 'main',
-  extname: '.hbs'
-});
-app.engine('hbs', hbs);
-app.set('view engine', 'hbs');
+  // app.get('/luke', async (req, res) => {
+  //   const fetchPeople = await fetch("https://swapi.co/api/people");
+  //   const peopleData = await fetchPeople.json();
+    
+  //   const [luke,C3PO,R2D2,DarthVader,Leia] = peopleData.results;
 
-/**
-* You can also refactor your routes like this later
-* @example  
-  const routes = require("./routes");
-  app.use("/api", routes);
-*/
+  //   console.log(luke);
+    
+  //   res.render('luke', {
+  //     person : luke,
+  //   })
+  
+  // })
 
-
-/**
-* Mock Data
-*/
-
-
-
-
-/** PAGES / VIEWS / ROUTES
-* Your pages or routes go here
-* 1. Route for your home page
-* 2. Route for your about page
-* 3. Route for any undefined page will send a 404 error.
-* 4. Handle errors
-*/
-app.get("/", (request, response) => {
-  response.render('index', {
-    exampleVariable: "Hello world"
-  });
-});
-app.get("/about", (request, response) => {
-  response.render('about');
-});
-
-
-
-/**
-* And we end with some more generic node stuff -- listening for requests :-)
-* console.log(process.env);
-*/
-let listener = app.listen(process.env.PORT, () => {
-  console.log('Your app is listening on port ' + listener.address().port);
-});
+  app.listen(8080);
